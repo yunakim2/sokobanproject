b613e82e8304f02856629d97523092ae28562de1
kimseoeyeon <ksy001109@naver.com>
2019-05-15 18:50:15 +0900

    소코반 게임완료화면 및 재시작 구현

--- 1.c
+++ 1.c
@@ -6,26 +6,79 @@
 #define UP 72
 #define DOWN 80
 #define MAXSTAGE 5
+#define MAX_NAME_LENGTH 10
 
 //함수 선언
-void LoadingStage();
-void DrawStage();
-void PlayerMoveAction();
-BOOL StageClear(); // 스테이지 완료함수
-void Printinfo();
+void login(); //로그인
+void startview(); //로딩
+void DrawStage(); //맵 불러오기
+void PlayerMoveAction(); //플레이어 이동
+BOOL StageClear(); // 스테이지 완료
+void Printinfo(); //게임정보출력
 void Re(); //재시작
 
 //전역변수 선언
 char Stage[18][21];
+char inputname[MAX_NAME_LENGTH];
 
 int RoundNum;
 int Roundx, Roundy;
 int NumMowe;
 int Restart;
 
+
 //로딩화면 출력
-void LoadingStage(){
-  /*이부분 채워주세욤*/
+void startview(){
+  printf("sokoban\n");
+	printf("Start....\n");
+	printf("\n\n");
+
+	login();
+	return ;
+}
+
+//로그인
+void login(){
+  int i =0, length = 0;
+	reinput : 	while(1)
+	{
+		printf("input name : ");
+		gets(inputname);
+		length = strlen(inputname);
+
+
+		// printf("inputname [0] = %c", inputname[0]);
+
+		for(i =0; i<=length ; i++)
+		{
+		  printf("inputname[i] : %c\n",inputname[i]);
+			if((inputname[i]>= 65 && inputname[i]<= 90) || (inputname[i]>=97 && inputname[i]<=122) ||
+					inputname[i] == '\0')
+			{
+				if(inputname[i] == '\0') {break;}
+
+				continue;
+			}
+
+			else
+			{
+				printf("영문자만 입력가능합니다. 다시 입력해주세요.\n");
+				goto reinput;
+			}
+
+		}
+		if(inputname[i] == '\0') {break;}
+	    if(length >MAX_NAME_LENGTH)
+		{
+			printf("inputname은 영문 최대 10문자만 가능합니다. 다시 입력해주세요. \n");
+
+			goto reinput ;
+		}
+
+
+	}
+	printf("Hello %s\n",inputname);
+	return ;
 }
 
 //맵 불러오기
@@ -82,13 +135,18 @@ while (1) {
   }
   if(StageClear()){ // 클리어 확인
     clrscr();
-    printf("%d Stage Clear!\n", RoundNum+1);
-    printf("Let's go Next Stage!\n");
-    delay(3500);
     if(RoundNum < MAXSTAGE - 1){ // 다음 스테이지 출력
+      printf("%d Stage Clear!\n", RoundNum+1);
+      printf("Let's go Next Stage!\n");
+      delay(3500);
       RoundNum++;
+      break;
     }
-    break;
+    else if(RoundNum == (MAXSTAGE - 1)){
+      printf("★★★★★★You clear all stages!★★★★★★\n");
+      printf("★★★★★★★★★★★Conglatulate★★★★★★★★★★★")
+
+    }
   }
 }
   getch();
