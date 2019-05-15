5dc64c16f73aac7a1fd3655b445f936625a8c2df
yunakim2 <98_02@naver.com>
2019-05-15 23:15:59 +0900

    keybordinput 함수 다시 구현 , 소코반ex4 오류 수정

--- sokobanex5.c
+++ sokobanex5.c
@@ -9,18 +9,26 @@ void login(); //inputname을 받는 함수
 void startview(); // 게임 시작 화면 출력 함수
 void savefile(); // sokoban 파일 저장함수
 void inputkeybord(); // 키보드 input 함수
-void rankingopen(); // ranking 출력 함수
+void totalrankingopen(); // totalranking 출력 함수
+void levelrankingopen(char c); // char c level 출력함수
 void rankingsave(); // ranking 저장 함수
 void rankingcompare(); // 움직인 횟수 순위 비교 함수
+_Bool StageClear(); // 스테이지 완료
+void Re(); //재시작
+void myflush(); // 입력버퍼 모두 지우는 함수
+
+/*플레이어 이동부분 맨아래 부분 확인해서 변경해주세요 - 완료 */
+/*메인함수에 함수이름 임의로 쓴거 있으니 확인해주세요*/
 
 #define MAX_NAME_LENGTH 10
 #define RANK_SIZE 5
 
 char inputname[MAX_NAME_LENGTH]; // inputname 저장
-FILE *save; 
+FILE *save;
 FILE *rankopen;
 FILE *ranksave;
 int usercount = 0; // 움직인횟수
+int Restart; //재시작 확인용
 
 typedef struct rankdata
 {
@@ -53,6 +61,33 @@ Userdata userdata;
 int main(void)
 {
 	startview();
+
+	//4번 5번
+	while (1) {
+	  //DrawStage(); //스테이지 출력 함수 <- 나중에 바꿔주세욤
+	  //Printinfo(); // 정보출력 함수 <- 나중에 바꿔주세욤
+	  //PlayerMoveAction(); // 플레이어 이동 함수 <- 나중에 바꿔주세욤
+	  if(Restart == 1){ // 재시작
+	    Restart = 0;
+	    break;
+	  }
+	  if(StageClear()){ // 클리어 확인
+	    clrscr();
+	    if(RoundNum < MAXSTAGE - 1){ // 다음 스테이지 출력
+	      printf("%d Stage Clear!\n", RoundNum+1);
+	      printf("Let's go Next Stage!\n");
+	      delay(3500);
+	      RoundNum++;
+	      break;
+	    }
+	    else if(RoundNum == (MAXSTAGE - 1)){
+	      printf("★★★★★★You clear all stages!★★★★★★\n");
+	      printf("★★★★★★★★★★★Conglatulate★★★★★★★★★★★")
+				delay(3500);
+	      exit(0);
+	    }
+	  }
+	}
 	return 0;
 }
 
@@ -61,7 +96,7 @@ void startview()
 	printf("sokoban\n");
 	printf("Start....\n");
 	printf("\n\n");
-	
+
 	login();
 	return ;
 }
@@ -80,29 +115,29 @@ void login()
 		for(i =0; i<=length ; i++)
 		{
 		  //printf("inputname[i] : %c\n",inputname[i]);
-			if((inputname[i]>= 65 && inputname[i]<= 90) || (inputname[i]>=97				 && inputname[i]<=122) || inputname[i] == '\0')
+			if((inputname[i]>= 65 && inputname[i]<= 90) || (inputname[i]>=97&& inputname[i]<=122) || inputname[i] == '\0')
 			{
 				if(inputname[i] == '\0') {break;}
 
 				continue;
 			}
-			
+
 			else
 			{
 				printf("영문자만 입력가능합니다. 다시 입력해주세요.\n");
 				goto reinput;
 			}
-			
+
 		}
 		if(inputname[i] == '\0') {break;}
 	    if(length >MAX_NAME_LENGTH)
 		{
 			printf("inputname은 영문 최대 10문자만 가능합니다. 다시 입력해주세요. \n");
 
-			goto reinput ; 
+			goto reinput ;
 		}
-		
-		
+
+
 	}
 	printf("Hello %s\n",inputname);
 	memcpy(userdata.name , inputname, sizeof(inputname));
@@ -113,55 +148,69 @@ void login()
 }
 void inputkeybord()
 {
-	char c;
+	char c,ch;
 	while(1)
 	{ c = getchar(); // 수정 필요
-	  getchar(); //null문자 지우는 용도 수정 필요 _ 한문자만 받도록 
-	  //myflush() 구현 필요
+	  ch = getchar();
+
+	  if ( ch == '\n')
+	  {
+		  myflush();
+	  }
+	
 	 switch(c)
 		{
 			case 'h' :
-				userdata.count++; //test용
+				userdata.count++; //test용****playermoveaction구현
 				break;
 			case 'j' :
 				userdata.count++; // test용
 				break;
 			case 'k' :
 				userdata.count++; //test용
-				break; 
+				break;
 			case 'l' :
 				userdata.count++; //test용
 				break;
 			case 'n' :
 				break;
 			case 'e' :
+				printf("B Y E %s ... \n",userdata.name);
 				savefile ();
-			break;
+				break;
 			case 'f' :
 				break;
 			case 'd' :
 				break;
 			case 'r' :
+				Re();
 				break;
 			case 't' :
-				rankingopen(); //수정 필요함!!! ,t만 전체순위,
-					  		   // t다음 숫자 오면 해당 맵 순위
+				totalrankingopen(); //수정 필요함!!! ,t만 전체순위,
+				
+				 if(c=='t' && (ch != '\n')){
+       				
+           				levelrankingopen(ch);
+						myflush();
+						}
 				break;
 			case 's' :
 				savefile();
 				break;
-			
-			default : 
+
+			default :
 				printf("다른키를 입력하세요\n");
 				break;
-			}
-	 if(c=='e'){exit(0);}
+		}
 	}
+	if(c=='e'){exit(0);}
+	
 	return;
 }
-void rankingopen()
+
+void levelrankingopen(char c)
 {
-	
+
 	rankopen = fopen("ranking.txt","r");
 	if(rankopen == NULL)
 	{
@@ -170,7 +219,7 @@ void rankingopen()
 	}
 	while(1)
 	{
-	
+
 		if(feof(rankopen))
 			break;
 		for(int i =0; i<RANK_SIZE ; i++)
@@ -178,25 +227,58 @@ void rankingopen()
 			fscanf(rankopen, "%d %d %s %d", rankdata[i].level, rankdata[i].rank, rankdata[i].name , rankdata[i].count);
 
 		}
-		for(int i =0 ; i<RANK_SIZE ; i++)
-		{
-			fscanf(rankopen, "%d %s %d",totalrankdata[i].rank,totalrankdata[i].name, totalrankdata[i].count);
-		}
+		
 	}
-	
-	 fprintf(ranksave,"-------------------------------------------\n");
-     fprintf(ranksave,"레벨   순위        이름         움직인 횟수 \n");
-     fprintf(ranksave,"-------------------------------------------\n");
+
+	 printf("***********map %c 순위 입니다.*************\n");
+	 printf("-------------------------------------------\n");
+     printf("   순위        이름         움직인 횟수 \n");
+     printf("-------------------------------------------\n");
   for( int i = 0; i<5 ; i++)
      {
 
          printf("%-3d %-3d %-8s %-7d\n\n",rankdata[i].level, rankdata[i].rank,rankdata[i].name, rankdata[i].count);
 
      }
-     fprintf(ranksave,"-------------------------------------------\n");
-	
+     printf("------------------------------------------\n");
+
 	fclose(rankopen);
 	return ;
+}
+void totalrankingopen()
+{
+	rankopen = fopen("ranking.txt","r");
+	if(rankopen == NULL)
+	{
+		printf("ranking 파일 열기 실패 !\n");
+		return ;
+	}
+	while(1)
+	{
+		if(feof(rankopen))
+			break;
+		for(int i =0 ; i<RANK_SIZE ; i++)
+		{
+			fscanf(rankopen, "%d %s %d", totalrankdata[i].rank, totalrankdata[i].name, totalrankdata[i].count);
+		}
+		 printf("*********** total 순위 입니다.*************\n");
+         printf("-------------------------------------------\n");
+         printf("   순위        이름         움직인 횟수 \n");
+         printf("-------------------------------------------\n");
+
+		
+	  for( int i = 0; i<5 ; i++)
+	      {
+	
+	           printf(" %-3d     %-10s      %-7d\n\n",totalrankdata[i].rank,totalrankdata[i].name,totalrankdata[i].count);
+	
+	      }
+	    printf("------------------------------------------\n");
+	}
+	   fclose(rankopen);
+	   return ;
+
+	
 }
 void rankingsave()
 {
@@ -207,10 +289,10 @@ void rankingsave()
 	fprintf(ranksave,"레벨  순위   이름   움직인 횟수 \n");
 	fprintf(ranksave,"-------------------------------\n");
 	for( int i = 0; i<5 ; i++)
-	{	
-		
+	{
+
 		fprintf(ranksave, " %d    %d %-10s      %-7d\n\n",rankdata[i].level, rankdata[i].rank,rankdata[i].name, rankdata[i].count);
-	
+
 
 	}
 	fprintf(ranksave,"--------------------------------\n");
@@ -223,7 +305,7 @@ void rankingsave()
 		fprintf(ranksave," %-3d     %-10s      %-7d\n\n",totalrankdata[i].rank,totalrankdata[i].name,totalrankdata[i].count);
 	}
 
-	fprintf(ranksave,"--------------------------------\n");		
+	fprintf(ranksave,"--------------------------------\n");
 	fclose(ranksave);
 	printf("raking 파일이 저장되었습니다.\n");
 	return ;
@@ -246,7 +328,7 @@ void rankingcompare()
 
 
 	for(int i = 0 ; i<RANK_SIZE+1 ; i++)
-	{ 
+	{
 		rankdata[i].rank =1;
 		for(int j = 0; j<RANK_SIZE+1 ; j++)
 		{
@@ -277,7 +359,7 @@ void rankingcompare()
 			}
 	}
 
-		
+
 
 	return ;
 }
@@ -291,12 +373,49 @@ void savefile()
 	}
 	fprintf(save,"******* %s의 소코반 파일 입니다. ********\n",inputname);
 	fprintf(save,"inputname : %s, total count : %d , level : %d ",userdata.name, userdata.count, userdata.level);
-	
-	
+
+
 	fclose(save);
 	rankingsave();
 	printf("소코반 파일이 저장되었습니다.\n");
 
 	return ;
 }
+//플레이어 이동
+void PlayerMoveAction()
+{
+	; // 구현 필요
+	return ;
+}
+
+//스테이지 완료
+_Bool StageClear(){
+  int x, y;
+  for(x = 0; x < 20; x++){
+    for(y = 0; y < 18; y++){
+      if(MapData[RoundNum][x][y] == '.' && Stage[x][y] != 0){
+        return 0 ;
+      }
+    }
+  }
+  return 1 ;
+}
+
+//재시작
+void Re(){
+  Restart = 1;
+  return ;
+}
+// stdin 입력 버퍼 모두 지우기
+void myflush()
+{
+	while(getchar() != '\n')
+	{
+		;
+	}
+	return ;
+
+}
+
+
 
